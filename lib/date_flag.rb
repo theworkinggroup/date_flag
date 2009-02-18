307f883090404512f9100cc67256939e41677e14
Scott Tadman <tadman@grinder.local>
2009-02-18 15:27:35 -0500

    DateFlag plug-in skeleton

---
+++ lib/date_flag.rb
@@ -0,0 +1,30 @@
+module DateFlag
+  def date_flag(field, options =  { })
+    action = (options[:action] ? options[:action] : field.to_s.sub(/_at$/, '')).to_sym
+    query = (options[:query] and options[:query].to_sym or action)
+    
+    define_method(:"#{action}=") do |value|
+      unless (read_attribute(field))
+        case (value)
+        when nil, false, '', '0':
+          write_attribute(field, nil)
+        when DateTime, Date, Time:
+          write_attribute(field, value)
+        else
+          write_attribute(field, Time.now.utc)
+        end
+      end
+    end
+
+    define_method(:"#{action}?") do
+      !!(read_attribute(field))
+    end
+  
+    define_method(:"#{action}!") do
+      unless (read_attribute(field))
+        write_attribute(field, Time.now.utc)
+        save!
+      end
+    end
+  end
+end
---
+++ tasks/date_flag_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :date_flag do
+#   # Task goes here
+# end
