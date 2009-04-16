001093fe49aee0fe424b2abe713e03774b5268b9
tadman <github@tadman.ca>
2009-04-16 12:03:11 -0400

    Added documentation and examples, allowed action? query method to test against current time

--- lib/date_flag.rb
+++ lib/date_flag.rb
@@ -1,14 +1,32 @@
 module DateFlag
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
+  
+  # Usage:
+  #
+  # class MyModel < ActiveRecord::Base
+  #   date_flag :flagged_at, :action => :flag
+  # end
+  #
+  # m = MyModel.new
+  # m.flagged?     # => false
+  # m.flag!        # Assigns flag_at to current time
+  # m.flag = true  # Same as flag!
+  # m.flagged?     # => true
   
   def date_flag(field, options =  { })
     action = (options[:action] ? options[:action] : field.to_s.sub(/_at$/, '')).to_sym
     query = (options[:query] and options[:query].to_sym or action)
     
     define_method(:"#{action}=") do |value|
+      # The action= mutator method is used to manipulate the trigger time.
+      # Values of nil, false, empty string, '0' or 0 are presumed to be
+      # false and will nil out the time. A DateTime, Date or Time object
+      # will be saved as-is, and anything else will just assign the current
+      # time.
+      
       unless (read_attribute(field))
         case (value)
-        when nil, false, '', '0':
+        when nil, false, '', '0', 0:
           write_attribute(field, nil)
         when DateTime, Date, Time:
           write_attribute(field, value)
@@ -19,11 +37,23 @@ module DateFlag
     end
 
     define_method(:"#{action}?") do
-      !!(read_attribute(field))
+      # The action? accessor method will return true if the date is defined 
+      # and is prior to the current time, or false otherwise.
+      
+      value = read_attribute(field)
+      
+      value ? (value <= Time.now.utc) : false
     end
   
     define_method(:"#{action}!") do
-      unless (read_attribute(field))
+      # The action! method is used to set the trigger time. If the time is
+      # already defined and is in the past, then the time is left unchanged.
+      # If it is undefined or in the future, then the current time is
+      # substituted.
+      
+      value = read_attribute(field)
+      
+      unless (value and (value <= Time.now.utc))
         write_attribute(field, Time.now.utc)
         save!
       end
